ce3f817d0c36b2eec2d620f5868ccbf53d37f7e9
Martin Obrecht <martin.obrecht@yahoo.com.br>
2021-07-07 13:02:56 -0300

    Referential Integrity

--- SalesWebMVC/Controllers/SellersController.cs
+++ SalesWebMVC/Controllers/SellersController.cs
@@ -52,7 +52,7 @@ namespace SalesWebMVC.Controllers
         }
 
         public async Task<IActionResult> Delete(int? id)
-        {
+        {   
             if (id == null)
             {
                 return RedirectToAction(nameof(Error), new { message = "Id not provided"});
@@ -73,8 +73,18 @@ namespace SalesWebMVC.Controllers
         [AutoValidateAntiforgeryToken]
         public async Task<IActionResult> Delete(int id)
         {
-            await _sellerService.RemoveAsync(id);
-            return RedirectToAction(nameof(Index));
+            try
+            {
+                await _sellerService.RemoveAsync(id);
+                return RedirectToAction(nameof(Index));
+            }
+            catch (IntegrityException e)
+            {
+
+                return RedirectToAction(nameof(Error), new { message = e.Message });
+            }
+            
+
         }
 
         public async Task<IActionResult> Details(int? id)
